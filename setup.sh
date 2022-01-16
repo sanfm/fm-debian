7f4330c0f250b4bd9066a132b3e7c7504736f18b
fedemoles <fede22ms@gmail.com>
2022-01-16 14:34:29 +0100

    Update setup.sh & lightdm-gtk.greeter.conf

--- setup.sh
+++ setup.sh
@@ -7,17 +7,7 @@ apt update
 
 # Install base packages
 # https://wiki.archlinux.org/title/desktop_environment
-#apt install unzip picom bspwm polybar lightdm rofi kitty thunar sxhkd nitrogen lxpolkit lxappearance xorg lightdm-gtk-greeter vim -y
-
-apt install unzip curl picom bspwm polybar lightdm rofi kitty thunar sxhkd nitrogen lxpolkit lxappearance xorg git vim -y
-
-
-#install lightdm-webkit2-greeter
-echo 'deb http://download.opensuse.org/repositories/home:/antergos/Debian_9.0/ /' | sudo tee /etc/apt/sources.list.d/home:antergos.list
-curl -fsSL https://download.opensuse.org/repositories/home:antergos/Debian_9.0/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_antergos.gpg > /dev/null
-apt update
-apt install lightdm-webkit2-greeter -y
-
+apt install unzip picom bspwm polybar lightdm rofi kitty thunar sxhkd nitrogen lxpolkit lxappearance xorg lightdm-gtk-greeter git vim -y
 
 # Enable lightdm on start up
 sudo systemctl enable lightdm.service
@@ -31,8 +21,3 @@ chown -R $username .config
 chgrp -R $username .config
 
 
-# Install litarvan theme for lightdm-webkit2-greeter
-mkdir -p /usr/share/lightdm-webkit/themes/litarvan
-cd /usr/share/lightdm-webkit/themes/litarvan
-git clone https://github.com/Litarvan/lightdm-webkit-theme-litarvan.git
-./build.sh
